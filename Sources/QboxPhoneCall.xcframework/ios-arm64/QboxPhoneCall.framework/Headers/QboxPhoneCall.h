61ce37fe33d6a3720ed9521bf01376a4e7b08af1
nurken <xteclu@gmail.com>
2024-06-22 14:16:36 +0500

    0.1.0

---
+++ Sources/QboxPhoneCall.xcframework/ios-arm64/QboxPhoneCall.framework/Headers/QboxPhoneCall.h
@@ -0,0 +1,18 @@
+//
+//  QboxPhoneCall.h
+//  QboxPhoneCall
+//
+//  Created by Nurken Tileubergenov on 22.06.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QboxPhoneCall.
+FOUNDATION_EXPORT double QboxPhoneCallVersionNumber;
+
+//! Project version string for QboxPhoneCall.
+FOUNDATION_EXPORT const unsigned char QboxPhoneCallVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QboxPhoneCall/PublicHeader.h>
+
+
